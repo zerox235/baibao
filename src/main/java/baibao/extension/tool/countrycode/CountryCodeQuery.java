3f9888c6e3d748cddfbeecd5e145deb69bc8292a
Kahle <kahle@undefined>
2025-04-25 23:23:02 +0800

    [feat] 增加 国家/地区编码 查询功能（基于 百度百科 ISO 3166-1）

---
+++ src/main/java/baibao/extension/tool/countrycode/CountryCodeQuery.java
@@ -0,0 +1,10 @@
+package baibao.extension.tool.countrycode;
+
+import java.io.Serializable;
+
+/**
+ * 国家/地区编码（ISO 3166-1）查询对象.
+ * @author Kahle
+ */
+public class CountryCodeQuery implements Serializable {
+}
